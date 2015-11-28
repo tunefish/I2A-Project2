fbd191d5d014b3f390c9b43238bd383bbcf65392
tunefish <tunefish@users.noreply.github.com>
2015-11-28 14:58:31 -0500

    Started implementing add/remove file from filebase

--- index.c
+++ index.c
@@ -6,50 +6,94 @@
 #include "util.h"
 
 /*add file to database*/
-void add_file(char * file) {
-	//TODO: remove all stop words from file
-	//TODO: stem all remaining words
-	//TODO: create new indexed words
-	//TODO: update already indexed words
-	return;
+void add_file(database_p db, char * file) {
+	FILE *fb_file = fopen("filebase", "a");
+    if (!fb_file) {
+        printf("Error: filebase file not found\n");
+        return;
+    }
+    
+    fprintf(fb_file, "%s\n", file);
+    fclose(fb_file);
+    
+    // TODO: parse contents of file and add it to database
 }
 
 /*remove file from database*/
-void remove_file(char * file)
+void remove_file(database_p db, char * file)
 {
-	//TODO: search for indexed word containing file in database
-	//TODO: remove file from *documents[] in indexed word
-	return;
+	FILE *fb_file = fopen("filebase", "a");
+    if (!fb_file) {
+        printf("Error: filebase file not found\nFile not removed from filebase!\n");
+        return;
+    }
+    
+    char *f;
+    int remove_line = 0;
+    while ((f = read_line(fb_file))) {
+        if (strlen(f) == strlen(file) && !memcmp(f, file, strlen(f))) {
+            break;
+        }
+        
+        remove_line++;
+    }
+    
+    rewind(fb_file);
+    
+    // create copy of filebase in .fb_tmp_cpy without the line containing the file to be removed
+    FILE *fb_copy = fopen(".fb_tmp_cpy", "w");
+    char c;
+    int line = 0;
+    while ((c = getc(fb_file)) != EOF) {
+        // write all characters except the ones in the line containing the file to be removed
+        if (line != remove_line) {
+            putc(c, fb_copy);
+        }
+        
+        if (c == '\n') {
+            line++;
+        }
+    }
+    
+    fclose(fb_file);
+    fclose(fb_copy);
+    
+    // remove original filebase and replace it with new copy
+    remove("filebase");
+    rename(".fb_tmp_cpy", "filebase");
+    
+    
 }
 
 /*search database for indexed word. Return documents containing word*/
-indexed_word_p search_database(char * query) {
+indexed_word_p search_database(database_p db, char * query) {
 	//TODO: search database for indexed words
 	//TODO: return *documents[] from indexed word
 	return NULL;
 }
 
 /*rebuild list of indexed words and documents associated with them*/
-void rebuild_index()
-{
+void rebuild_index(database_p db) {
 	//TODO:
     return;
 }
 
-/*load a database that already contains indexed words*/
+/*
+ * Parses and loads contents of the index file into a database struct
+ */
 database_p load_database() {
 	FILE * db_file = fopen("index", "r");
     if (!db_file) {
         printf("Error: index file not found\n");
-        exit(1);
+        return NULL;
     }
     
     database_p db = (database_p) malloc(sizeof(database_t));
     db->words = NULL;
     
-    char * line;
-    char * stem;
-    char * doc;
+    char *line;
+    char *stem;
+    char *doc;
     while ((line = read_line(db_file))) {
         // get the stem
         stem = strtok(line, ":");
@@ -91,6 +135,9 @@ database_p load_database() {
 	return db;
 }
 
+/*
+ * Frees the memory occupied by a database struct
+ */
 void close_database(database_p db) {
     indexed_word_p w;
     while ((w = db->words)) {
--- index.h
+++ index.h
@@ -10,9 +10,9 @@ typedef struct database {
    indexed_word_p words;
 } database_t, *database_p;
 
-void add_file(char * file);
-void remove_file(char * file);
-indexed_word_p search_database(char * query);
-void rebuild_index();
+void add_file(database_p db, char * file);
+void remove_file(database_p db, char * file);
+indexed_word_p search_database(database_p, char * query);
+void rebuild_index(database_p db);
 database_p load_database();
 void close_database(database_p db);
