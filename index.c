6da66b25bf9b3c0a5def4d33e46a7223b0f397fb
tunefish <tunefish@users.noreply.github.com>
2015-11-28 14:23:51 -0500

    Implementing DB load / close

--- index.c
+++ index.c
@@ -2,41 +2,110 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "index.h"
+#include "util.h"
+
 /*add file to database*/
-void add_file(char *)
+void add_file(char * file)
 {
 	//TODO: remove all stop words from file
 	//TODO: stem all remaining words
 	//TODO: create new indexed words
 	//TODO: update already indexed words
-	return 0;
+	return;
 }
 
 /*remove file from database*/
-void remove_file(char *)
+void remove_file(char * file)
 {
 	//TODO: search for indexed word containing file in database
 	//TODO: remove file from *documents[] in indexed word
-	return 0;
+	return;
 }
 
 /*search database for indexed word. Return documents containing word*/
-char search_database(char *)
+indexed_word_p search_database(char * query)
 {
 	//TODO: search database for indexed word
 	//TODO: return *documents[] from indexed word
-	return char;
+	return NULL;
 }
 
 /*rebuild list of indexed words and documents associated with them*/
 void rebuild_index()
 {
 	//TODO:
+    return;
 }
 
 /*load a database that already contains indexed words*/
-database_p load_database()
-{
-	//TODO: 
-	return database_p;
+database_p load_database() {
+	FILE * db_file = fopen("index", "r");
+    if (!db_file) {
+        printf("Error: index file not found\n");
+        exit(1);
+    }
+    
+    database_p db = (database_p) malloc(sizeof(database_t));
+    db->words = NULL;
+    
+    char * line;
+    char * stem;
+    char * doc;
+    while ((line = read_line(db_file))) {
+        // get the stem
+        stem = strtok(line, ":");
+       
+        // ignore empty lines
+        if (!stem) {
+            continue;
+        }
+        
+        // create struct for stem
+        int struct_size = sizeof(indexed_word_t);
+        indexed_word_p w = (indexed_word_p) malloc(struct_size);
+        w->stem = (char *) malloc(strlen(stem) + 1);
+        memcpy(w->stem, stem, strlen(stem) + 1);
+        w->nr_docs = 0;
+        
+        // insert into db
+        w->next = db->words;
+        db->words = w;
+        
+        // get list of documents containing this stem
+        stem = strtok(NULL, ":");
+        
+        // read each document
+        doc = strtok(stem, "|");
+        while(doc != NULL) {
+            struct_size+= sizeof(char*);
+            w = (indexed_word_p) realloc(w, struct_size);
+            w->documents[w->nr_docs] = (char *) malloc(strlen(doc) + 1);
+            memcpy(w->documents[w->nr_docs], doc, strlen(doc) + 1);
+            w->nr_docs++;
+            
+            doc = strtok(NULL, "|");
+        }
+    }
+    
+    fclose(db_file);
+    
+	return db;
+}
+
+void close_database(database_p db) {
+    indexed_word_p w;
+    while ((w = db->words)) {
+        db->words = w->next;
+        
+        free(w->stem);
+        
+        int i;
+        for (i = 0; i < w->nr_docs; i++) {
+            free(w->documents[i]);
+        }
+        
+        free(w);
+    }
+    free(db);
 }
--- index.h
+++ index.h
@@ -1,16 +1,18 @@
-typedef struct database {
-   int count:
-   indexed_word_t words;
-} database_t, *database_p;
-
 typedef struct indexed_word {
-    struct indexed_words *next;
+    struct indexed_word *next;
+    int nr_docs;
+    char *stem;
     char *documents[];
 } indexed_word_t, *indexed_word_p;
 
-void add_file(char *);
-void remove_file(char *);
-char search_database(char *);
+typedef struct database {
+   int count;
+   indexed_word_p words;
+} database_t, *database_p;
+
+void add_file(char * file);
+void remove_file(char * file);
+indexed_word_p search_database(char * query);
 void rebuild_index();
 database_p load_database();
-
+void close_database(database_p db);
