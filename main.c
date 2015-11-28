663dfb8be19b7ee1e5ab3a7d27b8f6a8845c2cb9
tunefish <tunefish@users.noreply.github.com>
2015-11-28 14:22:22 -0500

    Adding utils file

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "index.c"
+#include "index.h"
+#include "util.h"
 
 int starts_with(char *str, char *pre);
 char *read_line(FILE *ptr);
@@ -56,61 +57,6 @@ int main(int argc, void *argv) {
  */
 int starts_with(char *str, char *pre) {
     return strncmp(pre, str, strlen(pre)) == 0;
-}
-
-/*
- * Reads a line
- */
-char *read_line(FILE *ptr) {
-    char *line = malloc(128), *linep = line;
-    size_t lenmax = 128, len = lenmax;
-    int c;
-
-    if (line == NULL) {
-        return NULL;
-    }
-
-    for (;;) {
-        c = fgetc(ptr);
-        
-        // end of string / file => return string
-        if(c == EOF) {
-            break;
-        }
-
-        // string buffer full => double the size
-        if (--len == 0) {
-            len = lenmax;
-            char * linen = realloc(linep, lenmax *= 2);
-
-            if (linen == NULL) {
-                free(linep);
-                return NULL;
-            }
-            
-            line = linen + (line - linep);
-            linep = linen;
-        }
-
-        // end of line => return string
-        if((*line++ = c) == '\n') {
-            break;
-        }
-    }
-
-    *line = '\0';
-    
-    // nothing read => return NULL
-    if (strlen(linep) == 0) {
-        return NULL;
-    }
-    
-    // remove newline characted at the end of the string
-    if (*(line - 1) == '\n') {
-        *(line - 1) = '\0';
-    }
-    
-    return linep;
 }
 
 /*
